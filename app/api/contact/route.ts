4080bc208aa37b414695fe09cd4b818fa93e9e51
Florian Gattung <florian@shellfire.de>
2025-02-28 14:52:30 +0000

    update ocntact form

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse } from 'next/server';
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import { NextResponse } from "next/server";
+import { exec } from "child_process";
+import { promisify } from "util";
 
 const execPromise = promisify(exec);
 
@@ -13,14 +13,14 @@ export async function POST(req: Request) {
     if (!name || !email || !message) {
       return NextResponse.json(
         { message: "Name, email, and message are required fields." },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
     // Format the email content
     const subject = `Contact Form Submission from ${name}`;
-    const companyInfo = company ? `Company: ${company}\n` : '';
-    const phoneInfo = phone ? `Phone: ${phone}\n` : '';
+    const companyInfo = company ? `Company: ${company}\n` : "";
+    const phoneInfo = phone ? `Phone: ${phone}\n` : "";
     const emailContent = `
 From: ${name} <${email}>
 ${companyInfo}${phoneInfo}
@@ -32,18 +32,18 @@ ${message}
     const safeEmailContent = emailContent.replace(/"/g, '\\"');
 
     // Prepare the sendmail command
-    const sendmailCommand = `echo "${safeEmailContent}" | sendmail -t -f"${email}" info@shellfire.de -s "${subject}"`;
+    const sendmailCommand = `echo "${safeEmailContent}" | sendmail -t -f"blogserver@shellfire.de" info@shellfire.de -s "${subject}"`;
 
     // Execute the sendmail command
     await execPromise(sendmailCommand);
 
     return NextResponse.json({ success: true });
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error("Error sending email:", error);
 
     return NextResponse.json(
       { message: "Failed to send email. Please try again later." },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
